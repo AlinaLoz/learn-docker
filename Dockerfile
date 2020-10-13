93688b42e4634bad71b929e49c1ddb5b49e26894
a.lozyuk <a.lozyuk@pixelplex.io>
2020-10-14 00:40:26 +0300

    docker images.ubuntu

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,14 @@
 FROM ubuntu:latest
+
+COPY . /app
+WORKDIR /app
+
+RUN apt-get update
+RUN apt-get install curl -y
+RUN curl -sL https://deb.nodesource.com/setup_12.x | bash
+RUN apt-get install --yes nodejs
+
+RUN npm install
+EXPOSE 3002
+CMD ["node", "server"]
+
--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const app = express();
 const port = process.env.PORT || 3002;
 const host = process.env.HOSTNAME || "0.0.0.0";
 
-app.get('/', (req, resp) => {
-  return resp.send('test');
+app.get('/', async (req, resp) => {
+  const qweqwe = await require('./data/test-data.json');
+  return resp.send('test' + qweqwe.test + new Date());
 });
 app.listen(port, host, () => {
   console.log(`Node.js API server is listening on http://${host}:${port}/`);
 });
+  
