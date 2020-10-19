e0abbbeb3110724efbb8df9dd71b2f400c224512
a.lozyuk <a.lozyuk@pixelplex.io>
2020-10-19 21:57:32 +0300

    update docker-compose.yaml

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 3003;
+const port = process.env.PORT || 3002;
 const host = process.env.HOSTNAME || "0.0.0.0";
 
 app.get('/', async (req, resp) => {
+  console.log(process.env.TEST_ENV);
   const qweqwe = await require('./data/test-data.json');
   return resp.send('test' + qweqwe.test + new Date());
 });
